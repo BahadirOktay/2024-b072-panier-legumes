bad48e94f4537678771d0ed6bc2d674b33e6c899
Oktay Bahadir <124797662+BahadirOktay@users.noreply.github.com>
2024-04-20 15:00:28 +0200

    Version_20.4.24

---
+++ src/components/LegumesFiche.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  legume: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h2> {{legume.nom}} </h2>
+    <div> {{legume.prix}} CHF </div>
+    <div> {{legume.couleur}} </div>
+  </div>
+</template>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
---
+++ src/views/LegumesListeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import LegumesFiche from '@/components/LegumesFiche.vue'
+</script>
+
+<template>
+  <h1>Nos légumes</h1>
+  <ul>
+    <li><LegumesFiche :legume="{nom: 'Poivron', prix: 5, couleur: 'Rouge'}"/></li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
