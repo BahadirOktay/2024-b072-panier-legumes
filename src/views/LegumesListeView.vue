f17f35d274755745a9b7831bb63554fe258a17f3
Oktay Bahadir <124797662+BahadirOktay@users.noreply.github.com>
2024-04-24 11:31:52 +0200

    Creation de 3 pages

--- src/views/LegumesListeView.vue
+++ src/views/LegumesListeView.vue
@@ -1,15 +1,18 @@
 <script setup>
-
 import LegumesFiche from '@/components/LegumesFiche.vue'
 </script>
 
 <template>
-  <h1>Nos légumes</h1>
+  <h1>Nos Légumes</h1>
   <ul>
     <li><LegumesFiche :legume="{nom: 'Poivron', prix: 5, couleur: 'Rouge'}"/></li>
   </ul>
 </template>
 
 <style scoped>
-
+ul {
+  padding-left: 0;
+  margin-top: 0;
+  list-style-type: none;
+}
 </style>
